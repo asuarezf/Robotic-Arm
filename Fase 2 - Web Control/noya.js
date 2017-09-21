893cf71ab0f59726b03a1abd76aa321f93e2318a
Ricardo Di Zio <11-11274@usb.ve>
2017-09-20 23:14:30 -0400

    interface

--- Fase 2 - Web Control/noya.js
+++ Fase 2 - Web Control/noya.js
@@ -38,3 +38,20 @@ function heartbeat(){
 setInterval(heartbeat, 33);
 
 //console.log(slider1.value);
+(function($){
+  $(function(){
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
+    $('.slider').slider();
+    $('.button-collapse').sideNav();
+	// Next slide
+	$('.carousel').carousel('next');
+	$('.carousel').carousel('next', 3); // Move next n times.
+	// Previous slide
+	$('.carousel').carousel('prev');
+	$('.carousel').carousel('prev', 4); // Move prev n times.
+	// Set to nth slide
+	$('.carousel').carousel('set', 4);
+    $('.carousel.carousel-slider').carousel({fullWidth: true});  
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
